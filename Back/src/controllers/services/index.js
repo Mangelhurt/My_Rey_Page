7d8244d3bccb3eb3f2c854bde61689cc6fb9bebe
Miguel Hurtado <mangelhurt@gmail.com>
2025-09-23 12:29:41 -0500

    Agregamos las carpetas del back

---
+++ Back/src/controllers/services/index.js
@@ -0,0 +1,4 @@
+let nombre = "Miguel";
+let apellido = "Hortado";
+let saludo = `Hola Mundo! mi nombre es ${nombre} y mi apellido es ${apellido}, y esta va a ser una pagina web`;
+console.log(saludo);
